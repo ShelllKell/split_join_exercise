94f091fbf882916265621f2e66d923fd09b7b010
Shelby Kelly <shelby.kelly@colorado.edu>
2014-06-19 10:25:57 -0600

    Adding warmup

--- split_join_warmup.rb
+++ split_join_warmup.rb
@@ -10,30 +10,73 @@ world_cup_news
 
 # enter your solutions inside the methods
 def goal(message)
+  goal_message = "GOOOOOOOAL!!!!"
+  goal_message * 2
 
 end
 
 def generate_an_array_of_teams(teams)
+  team_list = "Brazil, Mexico, Cameroon, Croatia, Netherlands, Chile, Australia, Spain, Germany, Ghana, US, Protugal"
+  teams_array = team_list.split (", ")
 
 end
 
 def number_of_teams(teams)
+  team_list = "Brazil, Mexico, Cameroon, Croatia, Netherlands, Chile, Australia, Spain, Germany, Ghana, US, Protugal"
+  teams_array = team_list.split (", ")
+  teams_array.length
 
 end
 
 def return_australia(teams)
-
+  team_list = "Brazil, Mexico, Cameroon, Croatia, Netherlands, Chile, Australia, Spain, Germany, Ghana, US, Protugal"
+  teams_array = team_list.split (", ")
+  teams_array[6]
 end
 
 def starts_with_C(teams)
+  team_list = "Brazil, Mexico, Cameroon, Croatia, Netherlands, Chile, Australia, Spain, Germany, Ghana, US, Protugal"
+  teams_array = team_list.split (", ")
+
+  teams = team_list.split(", ")
+  teams.select {|team| team[0] == 'C' }
 
 end
 
 def block_string_to_single_line(block_string)
 
+  long_string = <<-world_cup_news
+  Clint, Dempsey, put, the, U.S.,
+  ahead, in, the, first, minute,
+  of, its, World, Cup, opener.
+  world_cup_news
+
+  long_string.split("\n").join(",").split(",").join(" ").split(" ").join(" ")
+
 end
 
 def capitalize_every_third_word(block_string)
+
+  long_string = <<-world_cup_news
+  Clint, Dempsey, put, the, U.S.,
+  ahead, in, the, first, minute,
+  of, its, World, Cup, opener.
+  world_cup_news
+
+  sentence = long_string.split("\n").join(",").split(",").join(" ").split(" ").join(" ")
+
+  new_sentence = sentence.downcase.capitalize
+  array = []
+
+  new_sentence.split(" ").each_with_index do |word, index|
+    if index % 3 == 0
+      array.push(word.capitalize)
+    else
+      array.push(word)
+    end
+  end
+
+  array.join(" ")
 
 end
 
